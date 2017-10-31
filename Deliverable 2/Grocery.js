abe7260469a489040232fc7ccecf070826cd6ecd
kwells <wellskhalid@yahoo.com>
2017-10-31 19:20:09 -0400

    new message

---
+++ Deliverable 2/Grocery.js
@@ -0,0 +1,20 @@
+// var food = ['carrot','green pepper','tomatoes'];
+
+// groceries.forEach(function(groceries){
+//     var paragraph = document.createElement('p');
+//     paragraph.innerHTML = `the groceries is ${groceries}`;
+//     document.body.appendChild(paragraph);
+// });
+
+// var header = document.createElement('h1');
+// header.innerHTML = 'there are a total of ${colors.length} colors';
+// document.body.appendChild(header);
+
+// var paragraph= document.createElement('p')
+// paragraph.innerHTML = '${item.name}: ${item'
+
+function add (){
+    var input1 = document.getElementById("grocery");
+    var input2 = document.getElementById("cost");
+    console.log(input1.value +" " + input2.value);
+}
---
+++ Deliverable 2/jquery.js
@@ -0,0 +1,9 @@
+// $("li").filter(":even").css("background-color", "red");
+// console.log("hello")
+
+var $listItems = $('li')l;
+
+console.log($listItems);
+Array.from($listItems).forEach(function(li){
+    $(li).addclass(('red-background');
+});
